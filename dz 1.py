9a12113a1a8773582ea0169c3d44b07ea2d93139
lec0aby <dormih05@gmail.com>
2023-09-09 12:38:02 +0300

    dz1

---
+++ dz 1.py
@@ -0,0 +1,8 @@
+x=int(input('Введите первое число'))
+y=int(input('Введите второе число'))
+if x>y:
+    print('x','больще чем','y')
+elif x<y:
+    print('y','больще чем','x')
+if x==y:
+    print('x=y')
