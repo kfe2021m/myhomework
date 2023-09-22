e4eed7788b40d3a9732df0df7e66ca402372d01a
kfe2021m <143869750+kfe2021m@users.noreply.github.com>
2023-09-23 00:41:42 +0300

    Delete dz 1.1 ot 09.09.2023.py

--- dz 1.1 ot 09.09.2023.py
+++
@@ -1,8 +0,0 @@
-num1=float(input('Введите первое число'))
-num2=float(input('Введите второе число'))
-if num1>num2:
-    print('1-ое больше чем 2-ое')
-elif num1<num2:
-    print('2-ое больше чем 1-ое')
-if num1==num2:
-    print('1-ое=2-ое')
