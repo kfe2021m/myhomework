1676e0b689419c099f1f2e411bea6a7c533b1428
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-10-07 13:04:26 +0300

    /

--- dz1/tr.py
+++ dz1/tr.py
@@ -1,8 +1,10 @@
-a = float(input())
+''''a = float(input())
 b = float(input())
 if  a > b:
     print('pervoe bolshe')
 elif b > a:
     print('vtoroe bolshe')
 else:
-    print('ony ravnyi')
+    print('ony ravnyi')'''
+def f(n):
+    n+=1
