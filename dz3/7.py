7eed50f4cb1888e2c7edc43e252206b938408d24
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-10-14 14:00:35 +0300

    dz 3

--- dz3/7.py
+++ dz3/7.py
@@ -3,7 +3,6 @@ from collections import Counter
 text = input().split()
 def f(text):
     u = max(text, key=len)
-    c = Counter(text)
-    a = max(c, key=lambda x: c[x])
-    print('Самое длинное: ' + u + ',самое частое: ' + a)
+    c = Counter(text).most_common(1)[0][0]   # подсчет элементов
+    print('Самое длинное: ' + u + ',самое частое: ' + c)
 f(text)
