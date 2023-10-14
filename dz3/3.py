92c5cb379c063ef78763eac34a78342764cbe564
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-10-14 13:55:13 +0300

    dz 3

--- dz3/3.py
+++ dz3/3.py
@@ -1,8 +1,10 @@
-k=0
-n=int(input())
+
+
 def f(n):
-    k=0
-    for x in range(1,n+1):
-        k=k+x
-        print(x, end='')
+    fi = [0,1]
+    for x in range(2,n):
+        fi.append(fi[x-1]+fi[x-2])
+    print(' '.join(str(w) for w in fi[:n]))
+
+n=int(input())
 f(n)
