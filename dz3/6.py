35e3a9c182919dfcb707af462d533393008e51a2
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-10-14 14:36:59 +0300

    dz 3

--- dz3/6.py
+++ dz3/6.py
@@ -1,10 +1,12 @@
 def f(n):
     if n < 2:
         return False
-    for i in range(2, int(n ** 0.5)+1):
-        if n % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
             return False
     return True
+
+
 n = int(input())
 if f(n):
     print('Простое число')
