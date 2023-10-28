69b9c687ca9dc8a89ffed135bd32696158568cd9
lec0baby <dormih05@gmail.com>
2023-10-28 12:51:41 +0300

    dz

--- tests/tests_three.py
+++ tests/tests_three.py
@@ -1,7 +1,3 @@
-#<<<<<<< HEAD:tests/test_3.py
-#import pytest
-from typing import ByteString
-from dz4.d3 import f
 from dz4.three import f
 
 @pytest.mark.parametrize(
@@ -24,10 +20,4 @@ def test(x):
 )
 def testf(x):
     assert f(x) is False
-=======
-#from dz4.three import f
-#def test_f():
-  #  assert f([34, 'Hello, world!', False])
-   # assert f('12345')
-   # assert not f('1113')
-#>>>>>>> 323aa772fce197658a9628f4102f187aab79f566:tests/tests_three.py
+
--- tests/tests_two.py
+++ tests/tests_two.py
@@ -1,5 +1,3 @@
-#<<<<<<< HEAD:tests/test_2.py
-#import pytest
 from dz4.d2 import cn
 
 
@@ -13,10 +11,4 @@ from dz4.d2 import cn
 )
 def test(n, end):
     assert cn(n) == end
-#=======
-#from dz4.two import fac
-#def test_fac():
-   # assert fac(0) == 1
-   # assert fac(1) == 1
-    #assert fac(5) == 120
-#>>>>>>> 323aa772fce197658a9628f4102f187aab79f566:tests/tests_two.py
+
