5a10c9158dca8ed901ecc13ccd0e2a2690914dc8
StarWolfAW <evdryakin@gmail.com>
2024-02-17 15:11:49 +0300

    /

--- dz8/dz.py
+++ dz8/dz.py
@@ -5,14 +5,20 @@ class Time:
         self.seconds = seconds
 
     def input_time(self):
-        self.hours = int(input("Введите часы: "))
-        self.minutes = int(input("Введите минуты: "))
-        self.seconds = int(input("Введите секунды: "))
+        while True:
+            self.hours = int(input("Введите часы: "))
+            self.minutes = int(input("Введите минуты: "))
+            self.seconds = int(input("Введите секунды: "))
+            if self.hours > 23 or self.minutes > 59 or self.seconds > 59:
+                print('no')
+            else:
+                break
+            
 
     def __str__(self):
         return f"Время: {self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
 
-# t = Time()
-# t.input_time()
-# print(t)
+t = Time()
+t.input_time()
+print(t)
