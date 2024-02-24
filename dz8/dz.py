f7a6d1175ec32ad78e35cdd339d2ea546d00b11c
kfe2021m <kfe2021m@yandex.ru>
2024-02-24 13:00:33 +0300

    .

--- dz8/dz.py
+++ dz8/dz.py
@@ -9,16 +9,15 @@ class Time:
             self.hours = int(input("Введите часы: "))
             self.minutes = int(input("Введите минуты: "))
             self.seconds = int(input("Введите секунды: "))
-            if self.hours > 23 or self.minutes > 59 or self.seconds > 59:
-                print('no')
+            if self.hours > 23 or self.minutes > 59 or self.seconds > 59 or self.hours < 0 or self.minutes < 0 or self.seconds < 0:
+                print('ошибка')
             else:
                 break
             
 
     def __str__(self):
-        return f"Время: {self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
-
-t = Time()
-t.input_time()
-print(t)
+# t = Time()
+# t.input_time()
+# print(t)
